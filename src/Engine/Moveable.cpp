89cbbf867b98ad429589bc1c288df6e5c7846617
coda <coda@codeposer.net>
2014-10-15 20:00:10 +0900

    Sync

--- src/Engine/Moveable.cpp
+++ src/Engine/Moveable.cpp
@@ -58,7 +58,7 @@ namespace soan {
 
 		tmath::convert(m_orientation, rotation);
 
-		return (trans * rotation * sc);
+		return (sc * rotation * trans);
 
 	}
 
@@ -84,7 +84,7 @@ namespace soan {
 		// Create inverse translation matrix.
 		tmath::translate(-m_position, trans);
 
-		return (sc * rotation * trans);
+		return (trans * rotation * sc);
 	}
 
 	void Moveable::setPosition(xdl::xdl_float x, xdl::xdl_float y, xdl::xdl_float z) {
